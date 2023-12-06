fcd8ed1cf42cb271841ef7a429fd27adfd39eb9d
huseynalipanah <147069072+huseynalipanah@users.noreply.github.com>
2023-12-06 14:12:23 +0400

    Add files via upload

--- flags/src/App.jsx
+++ flags/src/App.jsx
@@ -3,8 +3,11 @@ import { v4 as uuidv4 } from "uuid";
 import "./App.scss";
 
 function App() {
+  
   const [data, setData] = useState([]);
-  const [search, setSearch] = useState('')
+  const [search, setSearch] = useState("");
+  const [darkMode, setDarkMode] = useState(false); // Dark mode state'i tanımlanıyor
+
   useEffect(() => {
     async function apiProducts() {
       let api = await fetch("https://restcountries.com/v3.1/all");
@@ -15,33 +18,52 @@ function App() {
     apiProducts();
   }, []);
 
+  const myFunction = () => {
+    document.body.classList.toggle("dark-mode")
+  };
+
   return (
-    <div className="App">
-    <nav>
-        <span className="where">
-          Where in the world?
-        </span>
-        <a href=""><i class="fa-regular fa-moon fa-fw"></i>Dark Mode</a>
+    <div className={`App ${darkMode ? "dark-mode" : ""}`}> {/* Dark mode state'ine bağlı olarak sınıf adını değiştiriyoruz */}
+      <nav>
+        <span className="where">Where in the world?</span>
+        <a href="#" onClick={myFunction}> {/* Fonksiyonu doğrudan çağırmak yerine referansını geçiyoruz */}
+          <i class="fa-regular fa-moon fa-fw"></i>Dark Mode
+        </a>
       </nav>
       <div className="search">
-        <input type="text" value={search} onChange={ e => setSearch(e.target.value)}/>
+        <i class="fa-solid fa-magnifying-glass"></i>
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Search for a country ..."
+        />
       </div>
-        <div className="cards">
-      {data.filter((x) => x.name.common.toLowerCase().includes(search.toLowerCase()))
-      .map((x) => (
-        <div key={uuidv4()} className="country">
-          <img src={x.flags.png} alt="" />
-          <div className="about">
-            <h3>{x.name.common}</h3>
-            <div className="stats">
-            <p>Population: <span>{x.population}</span></p>
-            <p>Region: <span>{x.region}</span></p>
-            <p>Capital: <span>{x.capital}</span></p>
+      <div className="cards">
+        {data
+          .filter((x) =>
+            x.name.common.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((x) => (
+            <div key={uuidv4()} className="country">
+              <img src={x.flags.png} alt="" />
+              <div className="about">
+                <h3>{x.name.common}</h3>
+                <div className="stats">
+                  <p>
+                    Population: <span>{x.population}</span>
+                  </p>
+                  <p>
+                    Region: <span>{x.region}</span>
+                  </p>
+                  <p>
+                    Capital: <span>{x.capital}</span>
+                  </p>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      ))}
-    </div>
+          ))}
+      </div>
     </div>
   );
 }
